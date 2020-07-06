d5a99ccda9b70179aaeb2d2ae487ad9ad92c182e
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-06 11:05:40 +0900

    add:修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,15 +3,19 @@
     <meta charset='utf-8'>
 </head>
 <body>
-<h1>ログインフォーム</h1>
-@isset($message)
-    <p style="color:red">{{ $message }}</p>
-@endisset
-<form name="loginform" action="/auth/login" method="post">
+<h1>ユーザー登録フォーム</h1>
+<form name="registform" action="/auth/register" method="post">
     {{ csrf_field() }}
-    メールアドレス：<input type="text" name="email" size="30"><br/>
-    パスワード：<input type="password" name="password" size="30"><br/>
-    <button type='submit' name='action' value='send'>ログイン</button>
+    名前：<input type="text" name="name" size="30">
+    <span>{{ $errors->first('name') }}</span><br />
+    メールアドレス：<input type="text" name="email" size="30">
+    <span>{{ $errors->first('email') }}</span><br />
+    パスワード：<input type="password" name="password" size="30">
+    <span>{{ $errors->first('password') }}</span><br />
+    パスワード（確認）：<input type="password" name="password_confirmation" size="30">
+    <span>{{ $errors->first('password_confirmation') }}</span><br />
+
+    <button type='submit' name='action' value='send'>送信</button>
 </form>
 </body>
 </html>
